f279073ea8992db9ed07a69ce2f8c40c9638f885
Johnnie Regalado <johnniereg@gmail.com>
2017-11-03 21:58:22 +0000

    Algorithm working, now building a guard statement to only accept an array filled with numbers.

--- stock-market.js
+++ stock-market.js
@@ -1,38 +1,53 @@
-// Input is an array of numbers, each number representing
-// the price of a given stock on a day.
-//
-// Sample input: [45, 24, 35, 31, 40, 38, 11]
-// Should yield: 16
-//
+
+/**
+ * Given an array of numbers that represent stock prices on given days
+ * returns the maximum profit possible from buying at the lowest and
+ * selling at the highest price.
+ *
+ * @param  {Array <numbers>} stockprices An array containing numbers.
+ * @return {number}             The maximum profit when there is profit. Or -1 when no profit.
+ */
 
 function maxProfits (stockprices) {
-
+  // Check that the input is an array.
+  if (array == false) {
+    return null;
+  }
   // If no profit then we want to return -1.
   var biggestProfit = -1;
-
-  // We only go to second last item in array
+  // Takes each price and compares it to the prices in the array.
   for (var i = 0; i < stockprices.length; i++) {
-
     var buyingPrice = stockprices[i];
-
+    // Compares the buying price against all upcoming stock prices.
+    // Loop starts at the same spot as i so we don't compare to previous days.
     for (var j = i; j < stockprices.length; j++) {
       var sellingPrice = stockprices[j];
+      var profit = 0;
+      // Only make a change if the buy price is lower than sell.
       if (buyingPrice < sellingPrice) {
-        var profit = sellingPrice - buyingPrice;
+        profit = sellingPrice - buyingPrice;
       }
+      // Only increase the max profit amount if the profit is higher than the current
+      // biggest profit.
       if (profit >= biggestProfit) {
         biggestProfit = profit;
       }
-
     }
-
   }
-
   return biggestProfit;
 }
 
-// console.log(maxProfits([1, 5, 10]));
+// Check that the input is a valid array filled with numbers.
 
-console.log("The result should be: 9", maxProfits([1, 5, 10]));
-console.log("The result should be: 16", maxProfits([45, 24, 35, 31, 40, 38, 11]));
-console.log("The result should be: -1", maxProfits([10, 9, 8, 7, 0]));
+function inputChecker (array) {
+  if (Array.isArray(array) == false) {
+    return false;
+  }
+
+
+}
+
+console.log(maxProfits("abcdefg"));
+
+
+
