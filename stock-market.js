664eee15cddd4801d5abab65a123fbeddcc0628f
Johnnie Regalado <johnniereg@gmail.com>
2017-11-03 21:22:46 +0000

    Second commit, building loops to iterate over the array and set prices. Not quite working yet.

--- stock-market.js
+++ stock-market.js
@@ -1,20 +1,33 @@
 // Input is an array of numbers, each number representing
 // the price of a given stock on a day.
+//
+// Sample input: [45, 24, 35, 31, 40, 38, 11]
+// Should yield: 16
+//
 
 function maxProfits (stockprices) {
 
-  var profit = 0;
-  var biggestProfit = 0;
+  // If no profit then we want to return -1.
+  var biggestProfit = -1;
 
-  for (var i = 0; i < strockprices.length; i++) {
+  // We only go to second last item in array
+  for (var i = 0; i < stockprices.length - 2; i++) {
 
+    var buyingPrice = stockprices[i];
 
+    for (var j = 0; j < stockprices.length -2; j++) {
+
+      var profit = stockprices[j] - buyingPrice;
+      // console.log(profit);
+      if (profit >= biggestProfit) {
+        biggestProfit = profit;
+      }
+
+    }
 
   }
 
-
-
-
-
   return biggestProfit;
 }
+
+console.log(maxProfits([1, 5, 10]));
