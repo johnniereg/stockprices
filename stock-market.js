c45ed86e19c250147de1a18f62813af398c41617
Johnnie Regalado <johnniereg@gmail.com>
2017-11-03 21:08:08 +0000

    First commit. Starting to build algorithm.

---
+++ stock-market.js
@@ -0,0 +1,20 @@
+// Input is an array of numbers, each number representing
+// the price of a given stock on a day.
+
+function maxProfits (stockprices) {
+
+  var profit = 0;
+  var biggestProfit = 0;
+
+  for (var i = 0; i < strockprices.length; i++) {
+
+
+
+  }
+
+
+
+
+
+  return biggestProfit;
+}
